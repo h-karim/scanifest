01114072d808bf94f832d2aac5955efe3b0c76fe
h-karim <karimh96@hotmail.com>
2021-02-23 05:54:31 -0500

    add edge detection before HoughLinesP

--- scanifest/skewness.py
+++ scanifest/skewness.py
@@ -1,14 +1,16 @@
 import cv2
 import numpy as np
+import math as m
 
 def detect_skew_angle(image):
     height, width= image.shape
-    lines = cv2.HoughLinesP(i, rho=1, theta = np.pi/180, 
-            threshold = 100, minLineLength=width/2, maxLineGap=20)
+    edges = cv2.Canny(image, 50, 200)
+    lines = cv2.HoughLinesP(edges, rho=1, theta = np.pi/180, 
+            threshold = 100, minLineLength=width/2, maxLineGap=10)
     angle = 0
     for line in lines:
         x1, y1, x2, y2 = line[0]
-        angle += np.arctan2( (y2 - y1), (x2 - x1))
+        angle += m.atan2( (y2 - y1), (x2 - x1))
     print(angle)
     mean_ang = angle/(len(lines))
     print(mean_ang*180/np.pi)
@@ -16,19 +18,21 @@ def detect_skew_angle(image):
 
 def draw_lines(image, lines):
     for line in lines:
-        print(line[0])
         x1,y1,x2,y2 = line[0]
         cv2.line(image,(x1,y1),(x2,y2), (255,0,0))
+    cv2.imshow('lines', image)
+    cv2.waitKey(0)
 def correct_skew(image, rho):
     height, width = image.shape[:2]
     center = (width/2, height/2)
-    M = cv2.getRotationMatrix2D(center, -rho, 1)
+    M = cv2.getRotationMatrix2D(center, rho, 1)
     rotated = cv2.warpAffine(image, M, (height, width))
     cv2.imshow('rotated', rotated)
     cv2.waitKey(0)
-    pass
+    return rotated
 
 i = cv2.imread('./data/m20.jpg', cv2.CV_8UC1)
 img_not = cv2.bitwise_not(i)
 angle, lines = detect_skew_angle(img_not)
+draw_lines(img_not, lines)
 correct_skew(i, angle) 
